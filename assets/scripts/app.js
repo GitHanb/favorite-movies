5e7bef29b89c613bce2367ece4e665299cb531b7
Henry Han <31779862+GitHanb@users.noreply.github.com>
2020-01-05 15:06:38 -0800

    Updated app.js and app.css
    1. added Add Movie button function
    2. added Cancel button function on addMovieModal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,27 @@ const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
 const startAddMovieButton = document.querySelector('header button');
 //const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+// const backdrop = document.body.firstElementChild;
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+}
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
+const cancelAddMovie = () => {
+    toggleMovieModal();
+}
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
