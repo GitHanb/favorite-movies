35c3f4a442eb2de27e1b06c5a69323c4b5936af0
Henry Han <31779862+GitHanb@users.noreply.github.com>
2020-01-05 15:28:19 -0800

    Update app.js
    added confirmAddMovie function with validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,24 +6,45 @@ const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 // const backdrop = document.body.firstElementChild;
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
 
 const toggleBackdrop = () => {
-    backdrop.classList.toggle('visible');
-}
-
-const toggleMovieModal = () => {
-    addMovieModal.classList.toggle('visible');
-    toggleBackdrop();
+  backdrop.classList.toggle('visible');
 };
 
-const cancelAddMovie = () => {
-    toggleMovieModal();
-}
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  const titleValue = userInputs[0].value;
+  const titleValue = userInputs[0].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +rating < 1 ||
+    +rating > 5
+  ) {
+    alert('Please enter valid values (ratings between 1 and 5).');
+  }
+};
 
 const backdropClickHandler = () => {
-    toggleMovieModal();
-}
+  toggleMovieModal();
+};
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
